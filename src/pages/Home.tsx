c2c3861363ec1d8dc35f52c9cdcde9a272fbfc0f
otavio escalante <77994942+OtavioEscalnte@users.noreply.github.com>
2021-07-12 23:47:41 -0300

    up
    lógica adicionada

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,35 +5,60 @@ import { Header } from '../components/Header';
 import { Task, TasksList } from '../components/TasksList';
 import { TodoInput } from '../components/TodoInput';
 
+
+
+
+
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+      const newTask = {
+        id: new Date().getTime(),
+        title: newTaskTitle,
+        done: false
+      }
+      
+      setTasks(oldTasks =>[...oldTasks, newTask])
+
+    }
+    function handleToggleTaskDone(id: number) {
+      const updateTasks = tasks.map(task => ({...task}))
+
+      const foundItem = updateTasks.find(item => item.id === id)
+
+      if(!foundItem)
+      return
+
+      foundItem.done = !foundItem.done
+      setTasks(updateTasks)
+      
+
+    }
+  
+    function handleRemoveTask(id: number) {
+     const updateTasks = tasks.filter(task => task.id !== id)
+
+      setTasks(updateTasks)
+    }
+  
+    return (
+      <View style={styles.container}>
+        <Header tasksCounter={tasks.length} />
+  
+        <TodoInput addTask={handleAddTask} />
+  
+        <TasksList 
+          tasks={tasks} 
+          toggleTaskDone={handleToggleTaskDone}
+          removeTask={handleRemoveTask} 
+        />
+      </View>
+    )
   }
 
-  function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
-  }
+  
 
-  function handleRemoveTask(id: number) {
-    //TODO - remove task from state
-  }
-
-  return (
-    <View style={styles.container}>
-      <Header tasksCounter={tasks.length} />
-
-      <TodoInput addTask={handleAddTask} />
-
-      <TasksList 
-        tasks={tasks} 
-        toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
-      />
-    </View>
-  )
-}
 
 const styles = StyleSheet.create({
   container: {
